a0522f8511673a91ff55cc58030a493df79d87c2
KevinRodriguezLima <krodriguezl@unsa.edu.pe>
2025-09-14 08:17:52 -0500

    Intentando subir las iamgenes

--- src/sections/ComingSoon.jsx
+++ src/sections/ComingSoon.jsx
@@ -1,8 +1,12 @@
+import logo from '/images/pataspepas.webp'
+import psLogo from '/images/ps-logo.svg'
+import xLogo from '/images/x-logo.svg'
+
 const ComingSoon = () => {
   return (
     <section className="entrance-message">
       <div className="h-full col-center gap-10">
-        <img src="/images/pataspepas.webp" alt="logo" className="entrance-logo" />
+        <img src={logo} alt="logo" className="entrance-logo" />
 
         <div className="text-wrapper">
           <h3 className="gradient-title">
@@ -11,8 +15,8 @@ const ComingSoon = () => {
         </div>
 
         <div className="flex-center gap-10">
-          <img src="/images/ps-logo.svg" className="md:w-32 w-20" />
-          <img src="/images/x-logo.svg" className="md:w-52 w-40" />
+          <img src={psLogo} className="md:w-32 w-20" />
+          <img src={xLogo} className="md:w-52 w-40" />
         </div>
       </div>
     </section>
