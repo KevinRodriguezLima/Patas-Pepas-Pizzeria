acb08e2a49a8bf836b4f73a744894bbef27c8d0d
KevinRodriguezLima <krodriguezl@unsa.edu.pe>
2025-09-14 07:26:40 -0500

    Subida inicial del proyecto Patas Pepas Pizzeria

--- src/sections/ComingSoon.jsx
+++ src/sections/ComingSoon.jsx
@@ -2,11 +2,11 @@ const ComingSoon = () => {
   return (
     <section className="entrance-message">
       <div className="h-full col-center gap-10">
-        <img src="/images/logo.webp" alt="logo" className="entrance-logo" />
+        <img src="/images/pataspepas.webp" alt="logo" className="entrance-logo" />
 
         <div className="text-wrapper">
           <h3 className="gradient-title">
-            Coming <br /> May 26th <br /> 2026
+            Estreno <br /> 15 de Octubre <br /> 2025
           </h3>
         </div>
 
